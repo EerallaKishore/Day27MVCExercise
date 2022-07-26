28bb40ab578b69af03c6acaab55cc9740b1077c9
Eeralla.Kishore <Eeralla.Kishore@ECXLC6427>
2022-07-26 07:51:12 +0530

    Add project files.

---
+++ Day27MVCExercise/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day27MVCExercise.Models;
+
+namespace Day27MVCExercise.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var category = GetCategories();
+            return View();
+        }
+        public List<Category> GetCategories()
+        {
+            return new List<Category>
+            {
+                new Category { Id=1, Name="AMD Ryzen 5"},
+                new Category { Id=2, Name="Water Bottle"},
+               
+            };
+        }
+
+    }
+}
---
+++ Day27MVCExercise/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day27MVCExercise.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Sku_No { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public decimal price { get; set; }
+        public bool isavailable { get; set; }
+
+        public DateTime CreatedDate { get; set; }= DateTime.Now;
+
+        public Category Category { get; set; }  
+         public int CategoryId { get; set; }
+
+
+    }
+}
